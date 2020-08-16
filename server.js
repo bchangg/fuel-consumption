06b511a55170a0f953847414561bdc446f2bf4b7
Brian Chang <changbhc@gmail.com>
2020-08-15 23:58:27 -0700

    add call to connect to db; add comments for later

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+const db = require("./db");
+
+// const groceries = "./routes/groceries";
+// const householdItems = "./routes/household_items";
+// const bills = "./routes/bills";
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use("/api/groceries", groceries);
+// app.use("/api/household", householdItems);
+// app.use("/api/bills", bills);
 
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
-});
-
-app.post("/api/world", (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
-  );
-});
+// app.get("/api/hello", (req, res) => {
+//   res.send({ express: "Hello From Express" });
+// });
+//
+// app.post("/api/world", (req, res) => {
+//   console.log(req.body);
+//   res.send(
+//     `I received your POST request. This is what you sent me: ${req.body.post}`
+//   );
+// });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
